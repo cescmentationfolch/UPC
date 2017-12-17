a28411fa5f987b8fc8708c4cfbc4bacc03154a98
Dean Zhu <deanzhu1@gmail.com>
2017-12-17 12:22:20 +0100

    updated subintervals

--- Q5/CalculNumeric/Practica2/P1/SimpsonAdaptat.m
+++ Q5/CalculNumeric/Practica2/P1/SimpsonAdaptat.m
@@ -6,7 +6,7 @@ S(1) = (f(x(1)) + 4*f(x(2)) + f(x(3))) * (x(2)-x(1))/3;
 S(2) = (f(x(3)) + 4*f(x(4)) + f(x(5))) * (x(4)-x(3))/3;
 
 E = abs(I - (S(1) + S(2)));
-if E > tol*(r-l) 
+if E > tol*(r-l)
     [L1, L2, P1] = SimpsonAdaptat(f, l, (l+r)/2, tol);
     [R1, R2, P2] = SimpsonAdaptat(f, (l+r)/2, r, tol);
     I = L1 + R1;
@@ -16,4 +16,3 @@ if E > tol*(r-l)
     return
 end
 end
-
