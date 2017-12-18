f7bf5162b419671b3e196f3f1e863100dfa90c05
Dean Zhu <deanzhu1@gmail.com>
2017-12-18 15:11:08 +0100

    first CN pdf version

--- Q5/CalculNumeric/Practica2/P1/Simpson.m
+++ Q5/CalculNumeric/Practica2/P1/Simpson.m
@@ -2,8 +2,8 @@ function res = Simpson(f, xnum)
 x = linspace(0, 2, xnum);
 res = 0;
 i = 1;
-while i = 1:xum
-              res = res + (f(x(i)) + 4*f((x(i)+x(i+1))/2) + f(x(i+1))) * (x(i+1)-x(i))/6;
+for i = 1:xnum-1
+            res = res + (f(x(i)) + 4*f((x(i)+x(i+1))/2) + f(x(i+1))) * (x(i+1)-x(i))/6;
 end
 
 end
--- Q5/CalculNumeric/Practica2/P1/main.m
+++ Q5/CalculNumeric/Practica2/P1/main.m
@@ -1,12 +1,16 @@
-interval = [4,8,16,32,64];
+interval = [4,8,16,32,64,128,256,512,1024,2048,4096,8192];
 f = @(x) sin(exp(2*x));
-value = Integral(f, 0, 2);
-for i in 1:5
+value = integral(f, 0, 2);
+for i = 1:12
     valTrapezi(i) = Trapezi(f, interval(i));
     eTrapezi(i) = abs(value - valTrapezi(i))/value;
-    valSimpson(i) = Trapezi(f, interval(i));
+    valSimpson(i) = Simpson(f, interval(i));
     eSimpson(i) = abs(value - valSimpson(i))/value;
 end
 
 figure
-plot(log(interval), log(eTrapezi), 'r-', log(interval), log(eSimpson), 'b*')
+plot(log2(interval), log2(eTrapezi), 'r+', log2(interval), log2(eSimpson), 'b*')
+grid on;
+legend('Metode del Trapezi', 'Quadratura de Simpson');
+xlabel('log2(Nombre de subintervals)');
+ylabel('log2(error)');
