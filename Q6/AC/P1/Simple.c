117fabbdbdda0f6c61a4657a4add2669c97470cb
Dean Zhu <deanzhu1@gmail.com>
2018-02-28 20:15:01 +0100

    Primera practica AC

---
+++ Q6/AC/P1/Simple.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+  
+  long long i, resultado=0;
+  float t1,t2;
+  t1 = GetTime();
+  for ( i=0; i<3000000; i++)
+    resultado=resultado+i;
+  t2 = GetTime();
+  printf("Resultado = %lld\n",resultado);
+  printf("Milisegundos = %9f\n", t2-t1);
+}
