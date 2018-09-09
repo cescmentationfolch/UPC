98b4de94866e2a7ce32c6fee1b2db7fffdba93e0
Dean Zhu <deanzhu1@gmail.com>
2018-09-09 16:36:23 +0200

    update

---
+++ Q6/PAR/Practica5/solver-omp-task.c
@@ -0,0 +1,75 @@
+#include "heat.h"
+
+/*
+ * Function to copy one matrix into another
+ */
+
+void copy_mat (double *u, double *v, unsigned sizex, unsigned sizey)
+{
+    for (int i=1; i<=sizex-2; i++)
+        for (int j=1; j<=sizey-2; j++)
+            v[ i*sizey+j ] = u[ i*sizey+j ];
+}
+
+/*
+ * Blocked Jacobi solver: one iteration step
+ */
+double relax_jacobi (double *u, double *utmp, unsigned sizex, unsigned sizey)
+{
+    double diff, sum=0.0;
+
+    int howmany=4;
+    for (int blockid = 0; blockid < howmany; ++blockid) {
+        int i_start = lowerb(blockid, howmany, sizex);
+        int i_end = upperb(blockid, howmany, sizex);
+        for (int i=max(1, i_start); i<= min(sizex-2, i_end); i++) {
+            for (int j=1; j<= sizey-2; j++) {
+                utmp[i*sizey+j]= 0.25 * ( u[ i*sizey     + (j-1) ]+  // left
+                                          u[ i*sizey     + (j+1) ]+  // right
+                                          u[ (i-1)*sizey + j     ]+  // top
+                                          u[ (i+1)*sizey + j     ]); // bottom
+                diff = utmp[i*sizey+j] - u[i*sizey + j];
+                sum += diff * diff;
+            }
+        }
+    }
+
+    return sum;
+}
+
+/*
+ * Blocked Gauss-Seidel solver: one iteration step
+ */
+double relax_gauss (double *u, unsigned sizex, unsigned sizey)
+{
+    double unew, diff, sum=0.0;
+    int howmany=omp_get_max_threads();
+    int dep[howmany+1][howmany+1];
+    #pragma omp parallel
+    #pragma omp single
+    for (int blockid = 0; blockid < howmany; ++blockid) {
+        for (int blockid1 = 0; blockid1 < howmany; ++blockid1) {
+            int i_start = lowerb(blockid, howmany, sizex);
+            int i_end = upperb(blockid, howmany, sizex);
+            int j_start = lowerb(blockid1, howmany, sizey);
+            int j_end = upperb(blockid1, howmany, sizey);
+            #pragma omp task depend(in: dep[blockid][blockid1+1], dep[blockid+1][blockid1])  depend(out: dep[blockid+1][blockid1+1])  private(unew, diff)
+            for (int i=max(1, i_start); i<= min(sizex-2, i_end); i++) {
+                double = 0;
+                for (int j=max(1, j_start); j<= min(sizey-2, j_end); j++) {
+                    unew= 0.25 * ( u[ i*sizey	+ (j-1) ]+  // left
+                                   u[ i*sizey	+ (j+1) ]+  // right
+                                   u[ (i-1)*sizey	+ j     ]+  // top
+                                   u[ (i+1)*sizey	+ j     ]); // bottom
+                    diff = unew - u[i*sizey+ j];
+                    aux += diff * diff;
+                    u[i*sizey+j]=unew;
+                }
+                #pragma atomic
+                sum += aux;
+            }
+        }
+    }
+
+    return sum;
+}
